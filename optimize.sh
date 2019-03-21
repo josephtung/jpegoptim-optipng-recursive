4f88ad5cb8a1d856f5f80be663902c2a89980c3c
josephtung <josephtung@users.noreply.github.com>
2019-03-21 13:40:02 +0800

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 optimize() {
-  jpegoptim *.jpg --strip-all --all-progressive
-  jpegoptim *.jpeg --strip-all --all-progressive
-  jpegoptim *.JPG --strip-all --all-progressive
+  jpegoptim *.jpg --strip-all --all-progressive --max=90
+  jpegoptim *.jpeg --strip-all --all-progressive --max=90
+  jpegoptim *.JPG --strip-all --all-progressive --max=90
+  optipng *.png
+  
   for i in *
   do
     if test -d $i
